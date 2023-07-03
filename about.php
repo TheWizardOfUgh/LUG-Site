c185d1d677f6d6b16edaf8446a9d7e8b8ceccb80
Sean Jilek <sjilek1@umbc.edu>
2023-07-03 02:06:32 -0400

    Finished Installfest page, started about page

--- about.php
+++ about.php
@@ -1,24 +1,150 @@
 <!doctype html>
 
 <?php
-$title = "Php Header Footer";                   
-include "header.php";                 
+$title = "Php Header Footer";
+include "header.php";
 ?>
 
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>UMBC LUG - About</title>
-  </head>
-  <body>
-    <div class="jumbotron text-center">
-  		<h1>We are a linux users group</h1>
-  		<p>We teach people how to break their pc</p>
-	</div>
-  </body>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>UMBC LUG - About</title>
+</head>
+
+<body>
+  <article class="card">
+    <div class="card-body">
+      <h1 class="card-title">About</h1>
+      <p>UMBC Linux Users Group is a student run Linux Users Group at the uniersity of Maryland, Baltimore County. We run bi-weekly meetings about Linux, Free Software, and other alternative opperating systems.</p>
+
+      <p>This semester our meeting format is changing to presentations! We are currently looking for more presenters! If you have an idea for a talk pop in the <a href="https://discord.gg/jgMqPtK2mg">discord</a></p>
+
+      <h1 class="card-title">Officers</h1>
+      <h4>President: <a href="mailto:sjilek1@umbc.edu">Sean Jilek</a></h4>
+      <h4>Vice President: <a href="mailto:zamoss1@umbc.edu">Zach Amoss</a></h4>
+      <h4>Treasurer: <a href="mailto:funk1@umbc.edu">Warren Funk</a></h4>
+      <h4>Advisor: <a href="mailto:tompkins@umbc.edu">Dr.Tompkins</a></h4>
+
+      <h1 class="card-title">Club History</h1>
+      <p> UMBC's Linux Users Group was founded in October 1997 by Morty Abzug. The running (and incomplete) list of of officers is below. If you have any info about the gaps in leadership please reach out!</p>
+
+      <div class="page-content page-container" id="page-content">
+        <div class="padding">
+          <div class="row container d-flex justify-content-center">
+
+            <div class="col-lg-8 grid-margin stretch-card">
+              <div class="card">
+                <div class="card-body">
+                  <h4 class="card-title">Basic Table</h4>
+                  <p class="card-description">
+                    Basic table with card
+                  </p>
+                  <div class="table-responsive">
+                    <table class="table">
+                      <thead>
+                        <tr>
+                          <th>Year</th>
+                          <th>President</th>
+                          <th>Vice President</th>
+                          <th>Treasurer</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>Oct 1997 - Dec 1997</td>
+                          <td>Morty Azbug</td>
+                          <td>?</td>
+                          <td>?</td>
+                        </tr>
+                        <tr>
+                          <td>Dec 1997 - Jan 2001</td>
+                          <td>Dan Pearson</td>
+                          <td>?</td>
+                          <td>?</td>
+                        </tr>
+                        <tr>
+                          <td>Jan 2001 - Jun 2002</td>
+                          <td>Dave Norum</td>
+                          <td>?</td>
+                          <td>?</td>
+                        </tr>
+                        <tr>
+                          <td>Jun 2002 - Apr 2004</td>
+                          <td>Steve Killen</td>
+                          <td>?</td>
+                          <td>?</td>
+                        </tr>
+                        <tr>
+                          <td>Apr 2004 - May 2006</td>
+                          <td>Jim Smyth</td>
+                          <td>Rob Rifkin</td>
+                          <td>Holly Bennet</td>
+                        </tr>
+                        <tr>
+                          <td>Apr 2004 - May 2006</td>
+                          <td>Jim Smyth</td>
+                          <td>Rob Rifkin</td>
+                          <td>Holly Bennet</td>
+                        </tr>
+                        <tr>
+                          <td>Apr 2004 - May 2006</td>
+                          <td>Jim Smyth</td>
+                          <td>Rob Rifkin</td>
+                          <td>Holly Bennet</td>
+                        </tr>
+                        <tr>
+                          <td>Apr 2004 - May 2006</td>
+                          <td>Jim Smyth</td>
+                          <td>Rob Rifkin</td>
+                          <td>Holly Bennet</td>
+                        </tr>
+                        <tr>
+                          <td>Apr 2004 - May 2006</td>
+                          <td>Jim Smyth</td>
+                          <td>Rob Rifkin</td>
+                          <td>Holly Bennet</td>
+                        </tr>
+                        <tr>
+                          <td>Apr 2004 - May 2006</td>
+                          <td>Jim Smyth</td>
+                          <td>Rob Rifkin</td>
+                          <td>Holly Bennet</td>
+                        </tr>
+                        <tr>
+                          <td>Apr 2004 - May 2006</td>
+                          <td>Jim Smyth</td>
+                          <td>Rob Rifkin</td>
+                          <td>Holly Bennet</td>
+                        </tr>
+                        <tr>
+                          <td>Apr 2004 - May 2006</td>
+                          <td>Jim Smyth</td>
+                          <td>Rob Rifkin</td>
+                          <td>Holly Bennet</td>
+                        </tr>
+                        <tr>
+                          <td>Apr 2004 - May 2006</td>
+                          <td>Jim Smyth</td>
+                          <td>Rob Rifkin</td>
+                          <td>Holly Bennet</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+  </article>
+</body>
+
 </html>
 
 <?php
-include "footer.php";                
+include "footer.php";
 ?>
--- installfest.php
+++ installfest.php
@@ -1,23 +1,37 @@
 <!doctype html>
-<?php                  
-include "header.php";                 
-?>
-
-
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>UMBC LUG - Get Involved</title>
-    
-  </head>
-  <body>
-    <div class="jumbotron text-center">
-        <h1>This is installfest</h1>
-        <p>Again install Linux</p>
-    </div>
-  </body>
-
 
 <?php
-include "footer.php";                
+$title = "Php Header Footer";
+include "header.php";
+?>
+
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>UMBC LUG - About</title>
+</head>
+
+<body>
+  <article class="card">
+    <div class="card-body">
+      <h1 class="card-title">When and Where</h1>
+      <p>We are not planning another Installfest just yet this semester, but we announce each Installfest several times on our <a href="https://discord.gg/jgMqPtK2mg">discord</a> well before it happens. Feel free to post on the list and ask, or to attend our weekly meetings.</p>
+      <h1 class="card-title">What is Installfest?</h1>
+      <p> Linux Users' Groups are not only communities for existing users of Linux to find camaraderie and support, but are also advocate organizations. We want to enable and empower individuals to take control of their computers by using free and open software.</p>
+      <p>Each semester, we run Installfest, where seasoned Linux users volunteer their time to help newbies install Linux on their computers. We provide a wide selection of Linux flavors, appropriate for people of all skill levels. All of the services and software involved are free.</p>
+      <h1 class="card-title">How do I get Linux Installed? / How does this Work?</h1>
+      <p>If you want Linux, and want us to perform the installation, then you should come to Installfest. As mentioned above, we are not planning to run an Installfest in the very near future, so you may want to come to our weekly meetings instead, or ask on the <a href="https://discord.gg/jgMqPtK2mg">discord</a> for when we plan to run our next Installfest. </p>
+      <p>Before Installfest, you should <b>back up any important data</b> or programs on your computer, even if you are looking to dual-boot Linux and your current operating system. In the time before Installfest, you may want to start reading about available Linux distributions, and forming an opinion on what flavor might be best for you.</p>
+      <p>You may also try out our demo laptops, which will be outfitted with common Linux flavors, to help you get a better idea what you might want out of Linux. Prior to starting installation with one of our volunteers, you'll be asked to sign a release form, acknowledging that we can't provide any sort of warranty on your installation. Afterwards, one of our volunteers will help you choose a Linux flavor, guide and you through installation, assist you in installing any software you might need, and give you information on how to use your new operating system and what do if you encounter problems.</p>
+      <P>Be aware that all of the installers at the event are volunteers, and we can't provide any guarantee that your system will work the way you want. Sometimes, installs can take more time than we have available at Installfest. If that happens, you can come to our weekly meetings, during our meetings to complete your install. </P>
+    </div>
+  </article>
+</body>
+
+</html>
+
+<?php
+include "footer.php";
 ?>
